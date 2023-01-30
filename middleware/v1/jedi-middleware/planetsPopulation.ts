f48d7e2d4fbf2b06a387b08c18867864b82315d6
Christian Dominguez <christian@redbonzai.com>
2023-01-30 14:57:22 -0600

    small things

--- middleware/v1/jedi-middleware/planetsPopulation.ts
+++ middleware/v1/jedi-middleware/planetsPopulation.ts
@@ -1,10 +1,14 @@
 import {RequestHandler} from "express";
 import {JediService} from "../../../services/jedi.service";
+import {APIError} from "../../../error-handling/messages";
 
 export const planetsPopulation: RequestHandler = async (req, res, next) => {
     const jedi = new JediService()
 
     await jedi.galaxyPlanetPopulation().then((planets: any) => {
         return res.status(200).json({data: planets})
+    }).catch(error => {
+        console.error(error.stack)
+        next(new APIError('Not Found', 'Error retrieving planet population data', 404))
     })
 }
